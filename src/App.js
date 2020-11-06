b01bb885256d8c1faa5ce6b422175f6cc63cd259
Marton Levay <marton.levay@h4-software.hu>
2020-11-06 21:12:39 +0100

    Kaizen

--- src/App.js
+++ src/App.js
@@ -1,82 +1,48 @@
 import "./box/Box";
 import Card from "./card/Card.jsx";
+import _ from "lodash";
+import React, {Component} from 'react';
 
-function App() {
-  const valuePairs = {
-    heart: 0,
-    diamond: 1,
-    club: 2,
-    spade: 3,
-    two: 2,
-    three: 3,
-    four: 4,
-    five: 5,
-    six: 6,
-    seven: 7,
-    eight: 8,
-    nine: 9,
-    ten: 10,
-    jack: 11,
-    queen: 12,
-    king: 13,
-    ace: 14,
-  };
-
-  const shuffle = (list) => {
-    let cursorIndex = list.length,
-      temporary,
-      random;
-    while (0 !== cursorIndex) {
-      random = Math.floor(Math.random() * cursorIndex);
-      cursorIndex -= 1;
-
-      temporary = list[cursorIndex];
-      list[cursorIndex] = list[random];
-      list[random] = temporary;
+export class App extends Component {
+    render() {
+        const colors = ["club", "diamond", "heart", "spade"];
+        const faces = [
+            "two",
+            "three",
+            "four",
+            "five",
+            "seven",
+            "six",
+            "eight",
+            "nine",
+            "ten",
+            "jack",
+            "queen",
+            "king",
+            "ace",
+        ];
+        let cards = [];
+        colors.forEach((color) => {
+            faces.forEach((face) => {
+                cards.push({color, face});
+            });
+        });
+        cards = _.shuffle(cards);
+        console.log(cards);
+        return (
+            <div className="App">
+              {
+                cards.map((card, index)=>(
+                    <Card key={index} {...{
+                      index,
+                      color: card.color,
+                      face: card.face,
+                    }}/>
+                ))
+              }
+            </div>
+        );
     }
-    return list;
-  };
-
-  const colors = ["club", "diamond", "heart", "spade"];
-  const faces = [
-    "two",
-    "three",
-    "four",
-    "five",
-    "seven",
-    "six",
-    "eight",
-    "nine",
-    "ten",
-    "jack",
-    "queen",
-    "king",
-    "ace",
-  ];
-  let cards = [];
-  colors.forEach((color) => {
-    faces.forEach((face) => {
-      cards.push({ color: color, face: face });
-    });
-  });
-  cards = shuffle(cards);
-
-  const realCards = [];
-
-  for (let i = 0; i < cards.length; i++) {
-    console.log(cards[i]);
-    realCards.push(
-      <Card
-        color={cards[i].color}
-        face={cards[i].face}
-        key={cards[i].color + cards[i].face}
-      />
-    );
-  }
-
-  console.log(realCards);
-
-  return <div className="App">{realCards}</div>;
 }
 
 export default App;
--- src/card/Card.jsx
+++ src/card/Card.jsx
@@ -1,22 +1,31 @@
 import "./Card.scss";
-import { colors, faces } from "./cardConstants.js";
+import {colors, faces} from "./cardConstants.js";
+import React, {Component} from 'react';
 
-const Card = ({ color, face }) => {
-  const root = process.env.PUBLIC_URL;
-  return (
-    <>
-      <img
-        src={root + colors[color]}
-        style={{ width: "10px", height: "10px" }}
-        alt={color}
-      />
-      <img
-        src={root + faces[face]}
-        style={{ width: "12px", height: "12px" }}
-        alt={face}
-      />
-    </>
-  );
-};
+export class Card extends Component {
+    render() {
+        const root = process.env.PUBLIC_URL;
+        const {color, face, index} = this.props;
+
+        return (
+            <>
+                <span style={{fontSize: 20, fontWeight: 'bold', margin: 5}}>- {index}: </span>
+                <span style={{border: '2px solid black', padding: 3}}>
+                <img
+                    src={root + colors[color]}
+                    style={{width: "10px", height: "10px"}}
+                    alt={color}
+                />
+                <img
+                    src={root + faces[face]}
+                    style={{width: "12px", height: "12px"}}
+                    alt={face}
+                />
+                </span>
+            </>
+        );
+    }
+}
 
 export default Card;
+
